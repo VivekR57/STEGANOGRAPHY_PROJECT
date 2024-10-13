3c7fc771ad5709bce9282e2feb381bb091f9b2d0
Vivek R <144864263+VivekR57@users.noreply.github.com>
2024-10-13 12:18:50 +0530

    Update encode.c

--- 4-SkeletonCode/encode.c
+++ 4-SkeletonCode/encode.c
@@ -118,7 +118,6 @@ Status check_capacity(EncodeInfo *encInfo)
     // Get the size of the source image
     uint image_capacity = get_image_size_for_bmp(encInfo->fptr_src_image);
     uint size_secret_file = get_file_size(encInfo->fptr_secret);
-   printf("HEllo world");
     
 
 }
